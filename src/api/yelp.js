d28f0eea8b449403fb7877d4f692398bed029b86
Karan Pratap Singh <karanpsi@cisco.com>
2023-03-08 20:21:21 +0530

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
+// TODO: insert API key
 import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer EFSq07FlfX1nWBr-q3t6admfsyQVAnxs4DiV-zCWpoV0qaEZuBJe4b-cGfFO4VDy6HjPdWpheaVQOoXRLUI0QzUZqacDMewNfw8bVYzxFtBUG2tsLrkXoJ44sFwEZHYx'
+        Authorization: 'Bearer <api_key_here>'
     }
 });
